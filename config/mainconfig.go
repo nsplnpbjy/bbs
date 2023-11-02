c0687026163b2e91f01bac7fda797806aa0c758c
comradegenrr <nsplnp@126.com>
2023-11-02 22:08:46 +0800

    add ratelimit

--- config/mainconfig.go
+++ config/mainconfig.go
@@ -1,6 +1,8 @@
 package config
 
 import (
+	"time"
+
 	"github.com/gin-gonic/gin"
 	"github.com/nsplnpbjy/bbs/midware"
 	"github.com/nsplnpbjy/bbs/service"
@@ -9,6 +11,7 @@ import (
 func InitEngine(path string) *gin.Engine {
 	r := gin.Default()
 	//在这里插入中间件
+	r.Use(midware.RateLimitMiddleWare(time.Second, 100, 100))
 	r.Use(midware.LogMidware())
 	cs := NewControllerSet()
 	//在这里插入controller
